55dc14ad5bb7e2b78d3251fe8aef2fc09c935272
Satria Adi Putra <satadii11@gmail.com>
2019-10-03 07:34:48 +0700

    Refactor index route

--- app/controller/index_controller.js
+++ app/controller/index_controller.js
@@ -1,4 +1,9 @@
+const jsonfile = require('jsonfile');
+
 const Datatest = require('../model/datatest');
+const Synapse = require('../model/synapse');
+const Preprocesing = require('../model/preprocessing');
+
 
 module.exports = {
   async getSentiments() {
@@ -7,5 +12,52 @@ module.exports = {
       Datatest.find({ 'comment.category': 'Negatif' }).countDocuments().exec()
     ]);
     return { positif, negatif };
+  },
+
+  async getComments() {
+    const datatest = await Datatest.aggregate([{
+      $group: {
+        _id: {
+          date: '$date'
+        },
+
+        count: {
+          $sum: 1
+        }
+      }
+    }]).exec();
+    return datatest;
+  },
+
+  async uploadSynapse(file) {
+    const obj = jsonfile.readFileSync(file.path)
+    
+    await Synapse.remove({}).exec();
+    const newSynapse = new Synapse({
+      synapse0: obj.synapse0,
+      synapse1: obj.synapse1,
+      words: obj.words
+    });
+
+    await newSynapse.save();
+    return {
+      status_code: 201,
+      message: "Success upload synapse json"
+    };
+  },
+
+  async uploadPs(file) {
+    const obj = jsonfile.readFileSync(file.path);
+  
+    await Preprocesing.remove();
+    const newPreprocessing = new Preprocesing({
+      data: obj
+    });
+    
+    await newPreprocessing.save()
+    return {
+      status_code: 201,
+      message: "Success upload processing json"
+    };
   }
 };
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,102 +1,23 @@
 const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
-const jsonfile = require('jsonfile');
 
-const synapse = require('../model/synapse');
 const ps = require('../model/preprocessing');
-const router = express.Router();
 
-var index = require('../controller/api/index');
+const storage = multer.diskStorage({
+  filename: (req, file, callback) => {
+    callback(null, file.originalname);
+  }
+});
+const upload = multer({ storage })
+
 const baseController = require('../controller/base_controller');
 const indexController = require('../controller/index_controller');
 
-router.get('/sentiment', baseController(req => indexController.getSentiments()));
-router.get('/comment', index.getComment);
-router.post('/upload-synapse', (req, res, next) => {
-  var storage = multer.diskStorage({
-    filename: function (req, file, callback) {
-      console.log(file);
-      callback(null, file.originalname);
-    }
-  });
-  var upload = multer({
-    storage: storage
-  }).single('json');
-  upload(req, res, function (err) {
-
-    if (err)
-      console.log(err);
-
-    console.log(req.file.path);
-
-    var file = req.file.path;
-    jsonfile.readFile(file, function (err, obj) {
-      const data = {
-        synapse0: obj.synapse0,
-        synapse1: obj.synapse1,
-        words: obj.words
-      }
-      synapse.remove({}, () => {
-        console.log('remove synapse');
-      });
-      var syn = new synapse(data);
-      syn.save((err) => {
-        if (err)
-          console.log(err);
-
-        res.json({
-          status_code: 201,
-          message: "Success upload synapse json"
-        });
-      });
-    })
-
-  });
-});
-
-router.post('/upload-ps', (req, res, next) => {
-
-  var storage = multer.diskStorage({
-
-    filename: function (req, file, callback) {
-      console.log(file);
-      callback(null, file.originalname);
-    }
-  });
-  var upload = multer({
-    storage: storage
-  }).single('json');
-  upload(req, res, function (err) {
-
-    if (err)
-      console.log(err);
-
-    console.log(req.file.path);
-
-
-
-    var file = req.file.path;
-    jsonfile.readFile(file, function (err, obj) {
-      const data = {
-        data: obj
-      }
-      ps.remove({}, () => {
-        console.log('remove ps');
-      });
-      var syn = new ps(data);
-      syn.save((err) => {
-        if (err)
-          console.log(err);
-
-        res.json({
-          status_code: 201,
-          message: "Success upload processing json"
-        });
-      });
-    })
-
-  });
-});
+const router = express.Router();
+router.get('/sentiment', baseController(() => indexController.getSentiments()));
+router.get('/comment', baseController(() => indexController.getComments()));
+router.post('/upload-synapse', upload.single('json'), baseController(req => indexController.uploadSynapse(req.file)))
+router.post('/upload-ps', upload.single('json'), baseController(req => indexController.uploadPs(req.file)));
 
 module.exports = router;
